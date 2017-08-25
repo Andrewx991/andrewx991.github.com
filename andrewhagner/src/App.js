a72c1426a6193ec1b65fa7ae0e4fafa196c75fe2
Andrew Hagner <Andrewx991@gmail.com>
2017-08-25 09:32:25 -0400

    Worked on title layout and style.

--- andrewhagner/src/App.js
+++ andrewhagner/src/App.js
@@ -6,13 +6,13 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div className="pure-g">
-          <div className="Nav pure-u-1 pure-u-md-1-4">
-              Andrew Hagner <br/>
-              <img src={logo} />
+        <div className="pure-g title">
+          <div className="pure-u-1 pure-u-md-1-2">
+              <img src={logo} alt="Andrew Hagner" className="pure-img img-center"/>
           </div>
-          <div className="Content pure-u-1 pure-u-md-3-4">
-              Software Developer
+          <div className="pure-u-1 pure-u-md-1-2 text-center">
+            <h1 style={{marginBottom: '0.25em'}}>Andrew Hagner</h1>
+            <span>Technology, Architecture, and Logistics Consulting</span>
           </div>
         </div>
       </div>
